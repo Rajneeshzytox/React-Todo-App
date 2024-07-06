a38f7172b0cdf6b10ec833544518085f68f7cff8
Rajneesh Kumar <rajneesh.coder@gmail.com>
2024-07-06 21:36:07 +0530

    completed layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-
+import Sidebar from "./sections/Sidebar"
+import Navbar from "./sections/Navbar"
+import Col_1 from "./sections/Col_1"
+import Col_2 from "./sections/Col_2"
+import Col_3 from "./sections/Col_3"
+import Footer from "./sections/Footer"
 
 function App() {
-  return <button className="btn btn-primary">click me</button>
+  return (
+    <>
+    <div className="todo-layout">
+      <Sidebar />
+      <Navbar />
+      <Col_1 />
+      <Col_2 />
+      <Col_3 />
+      <Footer />
+    </div>
+    </>
+  )
 }
 
 export default App
---
+++ src/data/data.jsx
@@ -0,0 +1,10 @@
+export const col1 = [
+    {
+        id: 1,
+        img: "",
+        heading: "heading 1",
+        text: "text discripttion",
+        btnText: "btn",
+    }
+
+]
